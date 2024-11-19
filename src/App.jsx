3c4c5e16743ce64169d8630887ad0cffa5426487
feedan4 <fidanbagirova2004@gmail.com>
2024-11-19 11:15:06 +0400

    deneme

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import Header from './components/header/Header'
+import Main from './components/main/Main'
+import Footer from './components/footer/Footer'
 
 function App() {
   return (
-    <div>App</div>
+    <>
+      <Header />
+      <Main />
+      <Footer />
+    </>
   )
 }
 
