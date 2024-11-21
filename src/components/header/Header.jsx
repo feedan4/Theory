c1542f491431ed35443f65f6758678f84e2b4049
feedan4 <fidanbagirova2004@gmail.com>
2024-11-21 17:54:12 +0400

    biraz header biraz main

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,24 +1,41 @@
 import React, { useContext, useEffect } from 'react'
 import { DATA } from '../../context/DataContext'
+import { Link } from 'react-router-dom'
+import { SlMagnifier } from 'react-icons/sl'
+import { BsBasket3Fill } from 'react-icons/bs'
 
 function Header() {
   const { category } = useContext(DATA)
 
   return (
-    <div className='p-[20px] bg- hover:bg-white delay-300 flex justify-between items-center'>
+    <div className='px-[20px] py-[10px] bg-black flex justify-between items-center'>
       <div className='flex items-center'>
         <ul className='flex gap-4 items-center'>
           {
             category && category.map((item, index) => {
               return (
-                <li>{item.name}</li>
+                <li key={item.id} className='text-white text-[13px]'>{item.name}</li>
               )
             })
           }
         </ul>
       </div>
-      <div></div>
-      <div></div>
+      <div>
+        <img src='/img/theory.png' />
+      </div>
+      <div className='flex items-center gap-4'>
+        <div className='flex items-center'>
+          <SlMagnifier className='text-white text-[11px]'/>
+          <a href='#' className='text-white text-[13px]'>Search</a>
+        </div>
+        <a href='#' className='text-white text-[13px] capitalize'>Sign in/register</a>
+        <a href='#' className='text-white text-[13px]'>Search</a>
+        <a href='#' className='text-white text-[13px]'>Wishlist</a>
+        <div className='relative '>
+          <BsBasket3Fill className='text-white hover:text-black text-[13px]' />
+          <div className='absolute text-[13px]'>0</div>
+        </div>
+      </div>
     </div>
 
   )
--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -2,7 +2,19 @@ import React from 'react'
 
 function Main() {
   return (
-    <div></div>
+    <>
+    <section>
+      <video autoplay loop  muted className='w-full h-[86vh]' src='https://cdn.static.amplience.net/theory/_vid/dg_holiday_campaign_hp_hero_mobile/2fe64e60-41fb-4ab5-90dc-ab8c4972043a/video/17cb476c-da48-4131-956f-8da09e8a1466.mp4'></video>
+    </section>
+    <section className='flex items-center'>
+      <div className='section1'>
+
+      </div>
+      <div className='section2'>
+
+      </div>
+    </section>
+    </>
   )
 }
 
